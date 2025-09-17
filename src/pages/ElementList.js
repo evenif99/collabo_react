ffb4f918790a1f42a8eed4f3dff7cbfd2d40c0a0
evenif99 <back86538774@gmail.com>
2025-09-17 16:51:32 +0900

    collabo_react_elementtest

--- src/pages/ElementList.js
+++ src/pages/ElementList.js
@@ -4,17 +4,17 @@ import { API_BASE_URL } from "../config/config";
 import { Table } from "react-bootstrap";
 
 function App() {
-    const [fruitList, setFruitList] = useState([]); // 넘어온 과일 목록
+    const [elementList, setElementList] = useState([]); // 넘어온 과일 목록
 
     useEffect(() => {
-        const url = `${API_BASE_URL}/fruit/list`;
+        const url = `${API_BASE_URL}/element/list`;
 
         axios
             .get(url, {})
             .then((response) => {
                 // console.log(response.data);
 
-                setFruitList(response.data);
+                setElementList(response.data);
             });
 
     }, []);
@@ -27,14 +27,22 @@ function App() {
                         <th>아이디</th>
                         <th>상품명</th>
                         <th>단가</th>
+                        <th>카테고리</th>
+                        <th>수량</th>
+                        <th>이미지</th>
+                        <th>설명</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {fruitList.map((fruit) =>
+                    {elementList.map((fruit) =>
                         <tr key={fruit.id}>
                             <td>{fruit.id}</td>
                             <td>{fruit.name}</td>
                             <td>{Number(fruit.price).toLocaleString()}원</td>
+                            <td>{fruit.category}</td>
+                            <td>{Number(fruit.stock).toLocaleString()}</td>
+                            <td>{fruit.image}</td>
+                            <td>{fruit.description}</td>
                         </tr>
                     )}
                 </tbody>
--- src/pages/ElementOne.js
+++ src/pages/ElementOne.js
@@ -6,10 +6,10 @@ import { Table } from "react-bootstrap";
 
 // axios 라이브러리를 이용하여 리액트에서 스프링으로 데이터를 요청해야 합니다.
 function App() {
-    const [fruit, setFruit] = useState({}); // 넘겨 받은 과일 1개
+    const [element, setElement] = useState({}); // 넘겨 받은 원소 1개
 
     useEffect(() => { // BackEnd 서버에서 데이터 읽어 오기
-        const url = `${API_BASE_URL}/fruit`; // 요청할 url 주소
+        const url = `${API_BASE_URL}/element`; // 요청할 url 주소
 
         axios
             .get(url, {})
@@ -17,7 +17,7 @@ function App() {
                 console.log('응답 받은 데이터');
                 console.log(response.data);
 
-                setFruit(response.data);
+                setElement(response.data);
             });
     }, []);
 
@@ -27,15 +27,31 @@ function App() {
                 <tbody>
                     <tr>
                         <td>아이디</td>
-                        <td>{fruit.id}</td>
+                        <td>{element.id}</td>
                     </tr>
                     <tr>
                         <td>상품명</td>
-                        <td>{fruit.name}</td>
+                        <td>{element.name}</td>
                     </tr>
                     <tr>
                         <td>단가</td>
-                        <td>{Number(fruit.price).toLocaleString()}원</td>
+                        <td>{Number(element.price).toLocaleString()}</td>
+                    </tr>
+                    <tr>
+                        <td>카테고리</td>
+                        <td>{element.category}</td>
+                    </tr>
+                    <tr>
+                        <td>수량</td>
+                        <td>{Number(element.stock).toLocaleString()}</td>
+                    </tr>
+                    <tr>
+                        <td>이미지</td>
+                        <td>{element.image}</td>
+                    </tr>
+                    <tr>
+                        <td>설명</td>
+                        <td>{element.description}</td>
                     </tr>
                 </tbody>
             </Table>
